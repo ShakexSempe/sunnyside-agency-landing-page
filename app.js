373c3998eafb11022a927bcb5aa119794bdf6f32
Ray-Shakex <bshakex@gmail.com>
2021-08-25 12:06:15 +0200

    frontend report fixes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // nav menu toggle
 // select toggle element and links
-const toggle = document.getElementById('toggle');
+const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('.mobile-links');
 // toggle event listener to display menu on click of toggle element by adding class active
 toggle.addEventListener("click", () => {
