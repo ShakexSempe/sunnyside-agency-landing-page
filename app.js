598a1633c815ff02bb3fd473a8c22e85382ca5c8
Ray-Shakex <bshakex@gmail.com>
2022-08-24 12:59:52 +0200

    overlay

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('.aside-links');
 const headerLinks = document.querySelectorAll("header a");
+const container = document.querySelector('.container');
 console.log(headerLinks);
 // toggle event listener to display menu on click of toggle element by adding class active
 toggle.addEventListener("click", () => {
     nav.classList.toggle('active');
+    container.classList.toggle("overlay");
 });
 
 headerLinks.forEach(link => {
     link.addEventListener('click', () => {
     nav.classList.remove("active");
+    container.classList.remove("overlay");
     })
 })
 
