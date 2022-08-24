e58fcca1e612f27588061b7993038308f95b2c75
Ray-Shakex <bshakex@gmail.com>
2022-08-24 12:26:53 +0200

    header and hero layout

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // nav menu toggle
 // select toggle element and links
 const toggle = document.querySelector('.toggle');
-const nav = document.querySelector('.mobile-links');
+const nav = document.querySelector('.aside-links');
 // toggle event listener to display menu on click of toggle element by adding class active
 toggle.addEventListener("click", () => {
     nav.classList.toggle('active');
