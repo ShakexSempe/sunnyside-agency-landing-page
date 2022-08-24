92c07cf4de05663044874820e7ccf4ea48156ef8
Ray-Shakex <bshakex@gmail.com>
2022-08-24 12:53:48 +0200

    header links

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 // select toggle element and links
 const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('.aside-links');
+const headerLinks = document.querySelectorAll("header a");
+console.log(headerLinks);
 // toggle event listener to display menu on click of toggle element by adding class active
 toggle.addEventListener("click", () => {
     nav.classList.toggle('active');
+});
+
+headerLinks.forEach(link => {
+    link.addEventListener('click', () => {
+    nav.classList.remove("active");
+    })
 })
 
 // Date
